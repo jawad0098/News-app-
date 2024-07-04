fa0774cb8fa92f372257f0911d51eafa4961013e
jawad0098 <hassanjawad858@gmail.com>
2024-07-04 21:02:57 +0500

    commit

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -18,3 +18,23 @@ function openTab(evt, tabName) {
     evt.currentTarget.className += " active";
   }
   
+
+  function showTab(evt, sectionId) {
+    var i, tabSection, tabBtns;
+  
+    // Hide all tab content
+    tabSection = document.getElementsByClassName("tab-section");
+    for (i = 0; i < tabSection.length; i++) {
+      tabSection[i].style.display = "none";
+    }
+  
+    // Remove the active class from all tab buttons
+    tabBtns = document.getElementsByClassName("tab-btn");
+    for (i = 0; i < tabBtns.length; i++) {
+      tabBtns[i].className = tabBtns[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab and add an active class to the button
+    document.getElementById(sectionId).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
